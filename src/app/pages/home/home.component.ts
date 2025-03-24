c1bb74eb6e840d8da210d8951dedbb41ca977a6f
Lucas Silva <lucascarlosjobs@gmail.com>
2025-03-24 18:54:16 -0300

    feat(card): creates the base of the card component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { CardComponent } from "../../components/card/card.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
