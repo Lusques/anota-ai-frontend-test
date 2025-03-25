360b992cdafa5a4d15d1db04325f2526c4066ad7
Lucas Silva <lucascarlosjobs@gmail.com>
2025-03-25 14:03:28 -0300

    feat(searchbar): creates the base of the searchbar component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { CardComponent } from "../../components/card/card.component";
 import { CardWrapperComponent } from "../../components/card-wrapper/card-wrapper.component";
+import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, CardComponent, CardWrapperComponent],
+  imports: [HeaderComponent, CardComponent, CardWrapperComponent, SearchBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
